76c42948af1efbcbcbe59b4209f53f759f1c45e8
Juan J. Rojas <juanjorojash@gmail.com>
2025-01-22 13:27:06 -0600

    initial coomit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,138 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
 import math
 from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='SAR ADC dashboard',
+    page_icon=':level_slider:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
+def SAR(res,vref,vin,pr):
+# res = resolucion en formato 0bXXXXX
+# vref = voltaje de referencia positivo asumiendo 0 como referencia negativa
+# vin = voltaje de entrada, valor a convertir en digital
+# pr = imprimir o no
+    max = res+1 #0b1111 -> 0b10000
+    doc = max>>1 # 0b10000 -> 0b1000
+    if pr == True:
+        print("Step 0:", "{:04b}".format(doc) )
+    n = res.bit_length()
+    for index in range(1,n):
+        vdac = (doc/max)*vref
+        if n > 1:
+            doc = doc | (1 << n-2)
+        if vin < vdac:
+            doc = doc & ~(1 << n-1)
+        if pr == True:
+            print("Step %d:" % (index), "{:04b}".format(doc) )
+        n = n - 1
+    vout = (doc/max)*vref
+    return vout
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :level_slider: SAR ADC example
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+
+Change the "analog wave" and the resolution of the SAR ADC and observe the result
+
+
+Base frequency: 1000 Hz
+
+
+Amplitude: 1V
+
+
+Bias: 2V
 '''
 
 # Add some spacing
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+option = st.selectbox(
+    'Select the type of wave',
+    ['sine at 2f','sine at 2f + cosine at 4f'])
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+f = 1000
+T = 1/f
+t = np.arange(0,T,T/100)
+bias = 2 
+s = 0*t + bias
 
-countries = gdp_df['Country Code'].unique()
+match option:
+    case 'sine at 2f':
+        s = np.sin(2*np.pi*2*f*t) + bias
+    case 'sine at 2f + cosine at 4f':
+        s = np.sin(2*np.pi*2*f*t)+np.cos(2*np.pi*4*f*t) + bias
+    case _:
+        'ohh no'
 
-if not len(countries):
-    st.warning("Select at least one country")
+data={'time':t*1000,'wave':s}
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+df=pd.DataFrame(data)
 
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
+'Analog wave'
 st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+    df,
+    x='time',
+    y='wave',
+    x_label='time (ms)',
+    y_label='voltage (V)',
 )
 
-''
-''
+resolution = st.select_slider(
+    "Select the resolution of the ADC in bits",
+    options=[
+        "2",
+        "4",
+        "6",
+        "8",
+        "10",
+    ],
+)
+
+vout = np.array([])
+
+for index in range(len(s)):
+    match resolution:
+        case '2':
+            vout = np.append(vout, SAR(0b11,5,s[index],False))
+        case '4':
+            vout = np.append(vout, SAR(0b1111,5,s[index],False))
+        case '6':
+            vout = np.append(vout, SAR(0b111111,5,s[index],False))
+        case '8':
+            vout = np.append(vout, SAR(0b11111111,5,s[index],False))
+        case '10':
+            vout = np.append(vout, SAR(0b1111111111,5,s[index],False))
+
+df['dig'] = vout
+
+mse = mean_squared_error(df['wave'], df['dig'])
+
+'Analog wave with digital approximation'
+
+'RMSE:',mse
+st.line_chart(
+    df,
+    x='time',
+    y=['wave','dig'],
+    x_label='time (ms)',
+    y_label='voltage (V)',
+)
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
